ff98cbaa212764f9fe5fa98008bdf69a258cce77
ChiaLing Kuo <61963362+shawbu@users.noreply.github.com>
2020-06-23 16:56:46 +0800

    try

--- slider.js
+++ slider.js
@@ -2,43 +2,51 @@ $('.slider').each(function() {              // For every slider
     var $this   = $(this);                    // Current slider
     var $group  = $this.find('.slide-group'); // Get the slide-group (container)
     var $slides = $this.find('.slide');       // Create jQuery object to hold all slides
+    var slides_amount = $slides.length;       // Create jQuery object to hold all slides
     var buttonArray  = [];                    // Create array to hold navigation buttons
     var currentIndex = 0;                     // Hold index number of the current slide
     var timeout;         
       
-      // Sets gap between auto-sliding
-  
+
+    // Sets gap between auto-sliding
     function move(newIndex) {          // Creates the slide from old to new one
       var animateLeft, slideLeft;      // Declare variables
-  
       advance();                       // When slide moves, call advance() again
   
-      // If it is the current slide / animating do nothing
+      // 註解掉的話，如果在滑動過程又按下面的挑轉按鈕，就會出現bug
       if ($group.is(':animated') || currentIndex === newIndex) {  
         return;
       }
-  
+
+      // For button
       buttonArray[currentIndex].removeClass('active'); // Remove class from item
       buttonArray[newIndex].addClass('active');        // Add class to new item
   
-      if (newIndex > currentIndex) {   // If new item > current
-        slideLeft = '100%';            // Sit the new slide to the right
-        animateLeft = '-100%';         // Animate the current group to the left
-      } else {                         // Otherwise
-        slideLeft = '-100%';           // Sit the new slide to the left
-        animateLeft = '100%';          // Animate the current group to the right
-      }
+      // 預設 slideLeft ??%，到最後一個的時候變成 slideLeft -??%
+      // if (newIndex > currentIndex) {   // If new item > current
+        slideLeft = '33%';            // Sit the new slide to the right
+        animateLeft = '-33%';         // Animate the current group to the left
+      // } else {                         // Otherwise
+      //  slideLeft = '33%';           // Sit the new slide to the left
+      //  animateLeft = '-33%';          // Animate the current group to the right
+      // }
+
+      // For new picture
       // Position new slide to left (if less) or right (if more) of current
       $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
-  
+
+      console.log($slides.length);
+
+      // For old picture (== new-1)
       $group.animate( {left: animateLeft}, function() {    // Animate slides and
-        $slides.eq(currentIndex).css( {display: 'none'} ); // Hide previous slide      
+        $slides.eq(currentIndex).css( {left: -100,display: 'block',} ); // Hide previous slide
         $slides.eq(newIndex).css( {left: 0} ); // Set position of the new item
         $group.css( {left: 0} );               // Set position of group of slides
         currentIndex = newIndex;               // Set currentIndex to the new image
       });
     }
   
+    // Timeout
     function advance() {                     // Used to set 
       clearTimeout(timeout);                 // Clear previous timeout
       timeout = setTimeout(function() {      // Set new timer
@@ -47,9 +55,10 @@ $('.slider').each(function() {              // For every slider
         } else {                             // Otherwise
           move(0);                           // Move to the first slide
         }
-      }, 4000);                              // Milliseconds timer will wait
+      }, 2000);                              // Milliseconds timer will wait
     }
   
+    // For button
     $.each($slides, function(index) {
       // Create a button element for the button
       var $button = $('<button type="button" class="slide-btn">&bull;</button>'); //&bull是點的意思
@@ -62,7 +71,6 @@ $('.slider').each(function() {              // For every slider
       buttonArray.push($button);       // Add it to the button array
     });
   
+    // Trigger
     advance();                          // Script is set up, advance() to move it
-  
-  
   });
